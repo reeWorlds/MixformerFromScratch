849c3b56321ab79015c9a88e3886292bcc190112
unknown <max.koval4uk@ukr.net>
2024-04-14 21:45:40 +0300

    tmp fixp

--- Mixformer/Stage2_AE/Autoencoder.py
+++ Mixformer/Stage2_AE/Autoencoder.py
@@ -162,12 +162,9 @@ class Autoencoder(nn.Module):
         self.decoder = Decoder(config['decoder'])
 
     def forward(self, x, x_type):
-        print(f'x: {x.shape}, x_type: {x_type.shape}')
         x_type = x_type.to(x.device)
-        print(f'x: {x.shape}, x_type: {x_type.shape}')
         x = self.encoder(x, x_type)
         x = self.decoder(x, x_type)
-        print(f'x: {x.shape}, x_type: {x_type.shape}')
         return x
     
     def forward_encoder(self, x, x_type):
