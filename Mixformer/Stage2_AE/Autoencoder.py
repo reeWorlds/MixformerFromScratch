a61bf92d515f54c7272e75fd8b67aeb4dedd5015
unknown <max.koval4uk@ukr.net>
2024-04-15 23:38:37 +0300

    try to fix ae

--- Mixformer/Stage2_AE/Autoencoder.py
+++ Mixformer/Stage2_AE/Autoencoder.py
@@ -37,36 +37,25 @@ class Encoder(nn.Module):
         self.target_out_w = config["target_out_w"]
         self.search_out_h = config["search_out_h"]
         self.search_out_w = config["search_out_w"]
-        self.pos_enc_channels = config["pos_enc_channels"]
-        self.type_channels = config["type_channels"]
         self.embeddings = config["embeddings"]
-        self.agg_channels = config["agg_channels"]
-        self.agg_size = config["agg_size"]
+        self.reduce_size = config["reduce_size"]
 
         self.cnn1 = nn.Conv2d(3, self.part1_channels, kernel_size=5, stride=1, padding=2)
         self.block1 = nn.ModuleList([ResNetBlock(config["block1"]) for _ in range(2)])
         self.cnn2 = nn.Conv2d(self.part1_channels, self.part2_channels, kernel_size=3, stride=2, padding=1)
         self.block2 = nn.ModuleList([ResNetBlock(config["block2"]) for _ in range(2)])
+        self.drop1 = nn.Dropout(0.4)
         self.cnn3 = nn.Conv2d(self.part2_channels, self.part3_channels, kernel_size=3, stride=2, padding=1)
         self.block3 = nn.ModuleList([ResNetBlock(config["block3"]) for _ in range(3)])
+        self.drop2 = nn.Dropout(0.4)
         
-        target_weights = torch.randn(self.pos_enc_channels, self.target_out_h, self.target_out_w)
-        self.register_parameter('target_pos_embedding', nn.Parameter(target_weights))
-        search_weights = torch.randn(self.pos_enc_channels, self.search_out_h, self.search_out_w)
-        self.register_parameter('search_pos_embedding', nn.Parameter(search_weights))
-        self.type_embedding = nn.Embedding(2, self.type_channels)
-
-        self.agg_linear1 = nn.Linear(self.part3_channels, self.agg_channels)
-        self.target_agg_linear2 = nn.Linear(self.target_out_h * self.target_out_w * self.agg_channels,
-                                            self.agg_size)
-        self.search_agg_linear2 = nn.Linear(self.search_out_h * self.search_out_w * self.agg_channels,
-                                            self.agg_size)
-
-        self.drop1 = nn.Dropout(0.3)
-        self.linear1 = nn.Linear(self.part3_channels + self.pos_enc_channels + self.type_channels +
-                                 self.agg_size, self.embeddings)
-        self.linear2 = nn.Linear(self.embeddings, self.embeddings)
-        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.reduce1 = nn.Conv2d(self.part3_channels, self.reduce_size, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(self.reduce_size)
+        self.drop3 = nn.Dropout(0.4)
+        self.reduce2 = nn.Conv2d(16 * self.reduce_size, self.reduce_size, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(self.reduce_size)
+        self.linear_target = nn.Linear(self.reduce_size * 9, self.embeddings)
+        self.linear_search = nn.Linear(self.reduce_size * 16, self.embeddings)
 
     def forward(self, x, x_type):
         B = x.shape[0]
@@ -79,36 +68,21 @@ class Encoder(nn.Module):
         x = F.relu(self.cnn2(x))
         for block in self.block2:
             x = block(x)
+        x = self.drop1(x)
         x = F.relu(self.cnn3(x))
         for block in self.block3:
             x = block(x)
+        x = self.drop2(x)
 
-        y = rearrange(x, 'b c h w -> b h w c')
-        y = F.relu(self.agg_linear1(y))
-        y = y.view(B, -1)
+        x = self.bn1(F.relu(self.reduce1(x)))
+        x = rearrange(x, 'b c (h p1) (w p2) -> b (c p1 p2) h w', p1=4, p2=4)
+        x = self.drop3(x)
+        x = self.bn2(F.relu(self.reduce2(x)))
+        x = x.view(B, -1)
         if x_type.item() == 0:
-            y = self.target_agg_linear2(y).unsqueeze(-1).unsqueeze(-1)
-            y = y.repeat(1, 1, self.target_out_h, self.target_out_w)
+            x = self.linear_target(x)
         else:
-            y = self.search_agg_linear2(y).unsqueeze(-1).unsqueeze(-1)
-            y = y.repeat(1, 1, self.search_out_h, self.search_out_w)
-
-        type_embedding = self.type_embedding(x_type).repeat(B, 1).unsqueeze(-1).unsqueeze(-1)
-        if x_type.item() == 0:
-            pos_embedding = self.target_pos_embedding.repeat(B, 1, 1, 1)
-            type_embedding = type_embedding.repeat(1, 1, self.target_out_h, self.target_out_w)
-        else:
-            pos_embedding = self.search_pos_embedding.repeat(B, 1, 1, 1)
-            type_embedding = type_embedding.repeat(1, 1, self.search_out_h, self.search_out_w)
-
-        x = torch.cat([x, y, pos_embedding, type_embedding], dim=1)
-        x = rearrange(x, 'b c h w -> b h w c')
-
-        x = self.drop1(x)
-        x = F.relu(self.linear1(x))
-        x = self.linear2(x)
-        x = rearrange(x, 'b h w c -> b c h w')
-        x = self.global_pool(x).squeeze(-1).squeeze(-1)
+            x = self.linear_search(x)
 
         return x
 
@@ -125,36 +99,25 @@ class Decoder(nn.Module):
         self.target_out_w = config["target_out_w"]
         self.search_out_h = config["search_out_h"]
         self.search_out_w = config["search_out_w"]
-        self.agg_channels = config["agg_channels"]
-        self.agg_size = config["agg_size"]
-        self.pos_enc_channels = config["pos_enc_channels"]
-        self.type_channels = config["type_channels"]
         self.embeddings = config["embeddings"]
         self.part3_channels = config["part3_channels"]
         self.part2_channels = config["part2_channels"]
         self.part1_channels = config["part1_channels"]
+        self.reduce_size = config["reduce_size"]
 
-        self.agg_linear1 = nn.Linear(self.embeddings, self.agg_size)
-        self.target_agg_linear2 = nn.Linear(self.agg_size, self.target_out_h * self.target_out_w *
-                                            self.agg_channels)
-        self.search_agg_linear2 = nn.Linear(self.agg_size, self.search_out_h * self.search_out_w *
-                                            self.agg_channels)
-        self.agg_linear3 = nn.Linear(self.agg_channels, self.agg_size)
-
-        target_weights = torch.randn(self.pos_enc_channels, self.target_out_h, self.target_out_w)
-        self.register_parameter('target_pos_embedding', nn.Parameter(target_weights))
-        search_weights = torch.randn(self.pos_enc_channels, self.search_out_h, self.search_out_w)
-        self.register_parameter('search_pos_embedding', nn.Parameter(search_weights))
-        self.type_embedding = nn.Embedding(2, self.type_channels)
-
-        self.linear1 = nn.Linear(self.embeddings + self.pos_enc_channels + self.type_channels + self.agg_size,
-                                 self.embeddings)
-        self.drop1 = nn.Dropout(0.3)
-        self.linear2 = nn.Linear(self.embeddings, self.part3_channels)
+        self.linear_target = nn.Linear(self.embeddings, self.reduce_size * 9)
+        self.linear_search = nn.Linear(self.embeddings, self.reduce_size * 16)
+        self.upscale2 = nn.Conv2d(self.reduce_size, 16 * self.reduce_size, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(16 * self.reduce_size)
+        self.drop3 = nn.Dropout(0.4)
+        self.upscale1 = nn.Conv2d(self.reduce_size, self.part3_channels, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(self.part3_channels)
+        self.drop2 = nn.Dropout(0.4)
 
         self.block3 = nn.ModuleList([ResNetBlock(config["block3"]) for _ in range(3)])
         self.cnn3 = nn.Conv2d(self.part3_channels, self.part2_channels * 4, kernel_size=1, stride=1, padding=0)
         self.upsampler3 = nn.PixelShuffle(2)
+        self.drop1 = nn.Dropout(0.4)
         self.block2 = nn.ModuleList([ResNetBlock(config["block2"]) for _ in range(2)])
         self.cnn2 = nn.Conv2d(self.part2_channels, self.part1_channels * 4, kernel_size=1, stride=1, padding=0)
         self.upsampler2 = nn.PixelShuffle(2)
@@ -163,37 +126,23 @@ class Decoder(nn.Module):
 
     def forward(self, x, x_type):
         B = x.shape[0]
-        if x_type.item() == 0:
-            image_out_h, image_out_w = self.target_out_h, self.target_out_w
-        else:
-            image_out_h, image_out_w = self.search_out_h, self.search_out_w
-
-        y = F.relu(self.agg_linear1(x))
-        if x_type.item() == 0:
-            y = F.relu(self.target_agg_linear2(y)).view(B, image_out_h, image_out_w, self.agg_channels)
-        else:
-            y = F.relu(self.search_agg_linear2(y)).view(B, image_out_h, image_out_w, self.agg_channels)
-        y = self.agg_linear3(y)
-        y = rearrange(y, 'b h w c -> b c h w')
-
-        x = x.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, image_out_h, image_out_w)
-        if x_type.item() == 0:
-            pos_embedding = self.target_pos_embedding.repeat(B, 1, 1, 1)
-        else:
-            pos_embedding = self.search_pos_embedding.repeat(B, 1, 1, 1)
-        type_embedding = self.type_embedding(x_type).repeat(B, 1)
-        type_embedding = type_embedding.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, image_out_h, image_out_w)
         
-        x = torch.cat([x, y, pos_embedding, type_embedding], dim=1)
-        x = rearrange(x, 'b c h w -> b h w c')
-        x = F.relu(self.linear1(x))
-        x = self.drop1(x)
-        x = F.relu(self.linear2(x))
+        if x_type.item() == 0:
+            x = self.linear_target(x)
+            x = x.view(B, self.reduce_size, 3, 3)
+        else:
+            x = self.linear_search(x)
+            x = x.view(B, self.reduce_size, 4, 4)
+        x = self.bn2(F.relu(self.upscale2(x)))
+        x = rearrange(x, 'b (c p1 p2) h w -> b c (h p1) (w p2)', p1=4, p2=4)
+        x = self.drop3(x)
+        x = self.bn1(F.relu(self.upscale1(x)))
+        x = self.drop2(x)
 
-        x = rearrange(x, 'b h w c -> b c h w')
         for block in self.block3:
             x = block(x)
         x = F.relu(self.upsampler3(self.cnn3(x)))
+        x = self.drop1(x)
         for block in self.block2:
             x = block(x)
         x = F.relu(self.upsampler2(self.cnn2(x)))
--- Mixformer/Stage2_AE/ConfigGeneration.py
+++ Mixformer/Stage2_AE/ConfigGeneration.py
@@ -2,9 +2,9 @@ def make_ae_config():
     embed_dim = 256
     config = {"embeddings": embed_dim}
     coder_cfg = {"part1_channels": 64, "part2_channels": 80, "part3_channels": 96, "target_h": 48,
-               "target_w": 48, "search_h": 64, "search_w": 64, "pos_enc_channels": 32, "type_channels": 32,
-               "embeddings": embed_dim, "target_out_h": 12, "target_out_w": 12, "search_out_h": 16,
-               "search_out_w": 16, "agg_channels": 16, "agg_size": 32}
+               "target_w": 48, "search_h": 64, "search_w": 64, "embeddings": embed_dim,
+               "target_out_h": 12, "target_out_w": 12, "search_out_h": 16, "search_out_w": 16,
+               "reduce_size": 32}
     coder_cfg['block1'] = {"channels": 64, "mid_channels": 64}
     coder_cfg['block2'] = {"channels": 80, "mid_channels": 80}
     coder_cfg['block3'] = {"channels": 96, "mid_channels": 96}
